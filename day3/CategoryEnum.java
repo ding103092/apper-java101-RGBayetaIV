dfded0387ed528285f1db035d656b11e15d32842
Reginald Bayeta IV <reginald-g.bayeta@gcash.com>
2023-05-05 12:49:27 +0800

    Added enhanced switch in CategoryEnum

--- day3/CategoryEnum.java
+++ day3/CategoryEnum.java
@@ -5,29 +5,42 @@ public class CategoryEnum {
     }
 
     public static void main(String[] args) {
-        Category chosenCategory = Category.MEN;
+        Category chosenCategory = Category.WOMEN;
         double price = 0.0;
 
-        switch (chosenCategory) {
-            case MEN:
-                price = 13.57;
-                break;
-            case WOMEN:
-                price = 20.45;
-                break;
-            case KID:
-                price = 45.6;
-                break;
-            case GRANDMA:
-                price = 56.67;
-                break;
-            case GRANDPA:
-                price = 3.56;
-                break;
-            default:
-                System.out.println("Invalid category.");
-                break;
-        }
+        // Using enhanced switch (JAVA 12 and above only)
+        price = switch (chosenCategory) {
+            case MEN ->  13.57;
+            case WOMEN -> 20.45;
+            case KID -> 45.6;
+            case GRANDMA -> 56.67;
+            case GRANDPA -> 3.56;
+            // default -> -1; OPTIONAL, no default value was given
+            // by the instructions
+        };
+
+        // Using standard switch
+
+//        switch (chosenCategory) {
+//            case MEN:
+//                price = 13.57;
+//                break;
+//            case WOMEN:
+//                price = 20.45;
+//                break;
+//            case KID:
+//                price = 45.6;
+//                break;
+//            case GRANDMA:
+//                price = 56.67;
+//                break;
+//            case GRANDPA:
+//                price = 3.56;
+//                break;
+//            default:
+//                System.out.println("Invalid category.");
+//                break;
+//        }
 
         System.out.println("Price of the chosen category (" + chosenCategory + "): " + price);
     }
