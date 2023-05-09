27a2bcb7900bdc60f6cc348e76882ffce79e1d01
Reginald Bayeta IV <reginald-g.bayeta@gcash.com>
2023-05-09 10:20:56 +0800

    Added day 4 scratch and activity

---
+++ day4/day4-scratch/src/flight/App.java
@@ -0,0 +1,27 @@
+package flight;
+
+public class App {
+    public static void main(String[] args) {
+        Flight manilaToBoracay = new Flight();
+
+        for(int i = 0; i<10; i++) {
+            manilaToBoracay.addPassenger();
+        }
+
+        System.out.println("Current # of seats: " + manilaToBoracay.seats); // 7
+        System.out.println("Current # of passengers: " + manilaToBoracay.passengers); // 13
+
+        // Expected error message
+        manilaToBoracay.addPassenger();
+
+        // Now set to 20
+        manilaToBoracay.setNumberOfSeats(20);
+        for(int i = 0; i<3; i++) {
+            manilaToBoracay.addPassenger();
+        }
+
+        System.out.println("Current # of seats: " + manilaToBoracay.seats); // 7
+        System.out.println("Current # of passengers: " + manilaToBoracay.passengers); // 13
+
+    }
+}
---
+++ day4/day4-scratch/src/flight/Flight.java
@@ -0,0 +1,40 @@
+package flight;
+
+public class Flight {
+    // Fields
+    int passengers;
+    int seats;
+
+    // Constructors
+    Flight(int passengers, int seats) {
+        this.passengers = passengers;
+        this.seats = seats;
+    }
+
+    Flight() {
+        this.seats = 10;
+        this.passengers = 0;
+    }
+
+    // Methods
+    public void addPassenger() {
+        if(this.seats != 0) {
+            this.passengers++;
+            this.seats--;
+            // The ultimate debugger
+            // System.out.println(passengers + " " + this.seats);
+        }
+        else {
+            System.out.println("FULL!");
+        }
+    }
+
+    public void setNumberOfSeats(int seats) {
+        if(seats <= this.seats) {
+            System.out.println("Cannot allow seat numbers to be less or equal than current capacity.");
+        }
+        else {
+            this.seats = seats - passengers;
+        }
+    }
+}
